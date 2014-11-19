151f8da394b55938bdfca1b2b6471c9572b4ff76
Lorenz Wolf <lorenz.wolf@bluewin.ch>
2014-11-19 21:59:50 +0100

    ui design optimization

--- public/scripts/controllers/posts.detail.controller.js
+++ public/scripts/controllers/posts.detail.controller.js
@@ -1,6 +1,10 @@
-angular.module('redditclone').controller('postDetailController', function($scope, Socket, Post, $routeParams) {
+angular.module('redditclone').controller('postDetailController', function($scope, Socket, Post, $routeParams, $location) {
 
     var currentId = $routeParams.id;
     $scope.post = Post.get({ id : currentId});
 
+    //TODO: back as global function for each view
+    $scope.back = function(){
+        $location.path('/');
+    }
 });
