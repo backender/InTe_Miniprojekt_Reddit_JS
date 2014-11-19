41c66931c1958d875e2ce5dc6e59555670d80418
Marc Juchli <mail@marcjuch.li>
2014-11-19 14:14:27 +0100

    display error message from server

--- public/scripts/controllers/register.controller.js
+++ public/scripts/controllers/register.controller.js
@@ -1,19 +1,16 @@
 angular.module('redditclone').controller('registerController', function($scope, $location, User) {
 
-    $scope.userAlreadyExists = false;
-
-    $scope.login = function(credentials){
+    $scope.register = function(credentials){
 
         $scope.user = new User();
-
         $scope.user.name = credentials.username;
         $scope.user.password = credentials.password;
 
         $scope.user.$register(function(res){
             $location.path('/');
         },function(error){
-            //TODO: Bei Fehler macht in jedem Fall HTTP Status 200 (OK) und gibt bei Fehler nur ein false als Response zurück -> Besser: server umbauen damit er bei Fehler Error Code zurück gibt
-            $scope.userAlreadyExists = true;
+            console.log(error);
+            $scope.error= error;
         });
     }
 
